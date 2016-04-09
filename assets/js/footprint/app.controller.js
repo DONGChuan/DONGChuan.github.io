78f4406481d1c390c4900aeddf1e36639c1de6db
dchuan <dongchuan55@gmail.com>
2016-04-09 22:20:59 +0200

    Test angularjs

---
+++ assets/js/footprint/app.controller.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var app = angular.module('app');
+
+app.controller('footprintCtrl', footprintCtrl);
+
+function footprintCtrl() {
+
+    var self = this;
+
+    self.test = "test";
+}
